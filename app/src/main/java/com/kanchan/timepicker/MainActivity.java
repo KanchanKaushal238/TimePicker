642f27bd923153193a8cf73834d0ed107ee5bc0b
Kanchan Kaushal <kaushalkanchan238@gmail.com>
2020-02-10 22:01:06 +0530

    Use of Time Picker in Android Studio

---
+++ app/src/main/java/com/kanchan/timepicker/MainActivity.java
@@ -0,0 +1,46 @@
+package com.kanchan.timepicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+EditText time;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        time = findViewById(R.id.time);
+
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar mCurrentTime = Calendar.getInstance();
+                int hour = mCurrentTime.get(Calendar.HOUR_OF_DAY);
+                int min =  mCurrentTime.get(Calendar.MINUTE);
+
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        time.setText(hourOfDay+ ":"+ minute);
+                    }
+                }, hour, min, true);
+                mTimePicker.setTitle("select time");
+               mTimePicker.show();
+            }
+        });
+
+    }
+}
